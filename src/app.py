d374a5304e2040f9cbc0034d5622679c29739c4f
dianysof <103054678+dianysof@users.noreply.github.com>
2023-09-12 23:14:39 +0000

    done

--- src/app.py
+++ src/app.py
@@ -1,8 +1,35 @@
 import os
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
 
-
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 # your app code here
+
+
+
+con = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret))                                                             
+artist_id = "4q3ewBCX7sLwd24euuV69X"
+
+response = con.artist_top_tracks("4q3ewBCX7sLwd24euuV69X")
+if response:
+    tracks = response["tracks"]
+    tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]                                                             
+
+
+import pandas as pd
+
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+
+print(tracks_df.head(3))
+
+import seaborn as sns
+
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
